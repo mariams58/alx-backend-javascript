384f226a58c9e45c8714a5096bd1baf9f450b66b
mariams58 <mariams58@yahoo.com>
2024-10-04 17:47:21 +0100

    0x00 - js basics task 1& 2

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+  return [task, task2];
+}
---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+export default function getNeighborhoodList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
