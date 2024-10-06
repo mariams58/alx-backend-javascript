11a9f8ac888c5cf1fa8a511be31ec8aa55906cd7
mariams58 <mariams58@yahoo.com>
2024-10-06 10:12:26 +0100

    0x00 js-basics: task 12 fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,9 +2,9 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: {
       ...employeeList,
-    };
+    },
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
-    };
+    },
   };
 }
